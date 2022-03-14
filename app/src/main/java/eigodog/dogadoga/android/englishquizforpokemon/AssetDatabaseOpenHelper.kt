317ca66ea853bd2f7149f04ab4cc1d57bf3dc7cc
dogadoga <kishiouaruhureddo@gmail.com>
2022-03-14 23:22:44 +0900

    データベースをアセットからコピーするクラスを作成

---
+++ app/src/main/java/eigodog/dogadoga/android/englishquizforpokemon/AssetDatabaseOpenHelper.kt
@@ -0,0 +1,53 @@
+package eigodog.dogadoga.android.englishquizforpokemon
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+/**
+ * データベースをアセットからコピーする
+ */
+class AssetDatabaseOpenHelper (private val context: Context) {
+    companion object {
+        private const val DB_NAME = "pokemondata.sqlite3"
+    }
+
+    fun openDatabase(): SQLiteDatabase {
+        val dbFile = context.getDatabasePath(DB_NAME)
+
+
+        if (!dbFile.exists()) {
+            try {
+                val checkDB = context.openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null)
+
+                checkDB?.close()
+                copyDatabase(dbFile)
+            } catch (e: IOException) {
+                throw RuntimeException("Error creating source database", e)
+            }
+
+        }
+        return SQLiteDatabase.openDatabase(dbFile.path, null, SQLiteDatabase.OPEN_READWRITE)
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun copyDatabase(dbFile: File) {
+        val `is` = context.assets.open(DB_NAME)
+        val os = FileOutputStream(dbFile)
+
+        val buffer = ByteArray(1024)
+        while (`is`.read(buffer) > 0) {
+            os.write(buffer)
+            Log.d("#DB", "writing>>")
+        }
+
+        os.flush()
+        os.close()
+        `is`.close()
+        Log.d("#DB", "completed..")
+    }
+}
